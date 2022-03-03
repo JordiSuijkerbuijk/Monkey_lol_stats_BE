c22453216506582a086841b1c686e00a1a1505b0
Roel <rvoordendag@gmail.com>
2022-03-03 19:16:20 +0100

    Added firebase connect with DB util

--- app/server.ts
+++ app/server.ts
@@ -10,7 +10,7 @@ app.get('/', (req: Request, res: Response) => {
   res.send('Hello World!');
 });
 
-app.get('/firestore', (req:object, res:object) => firebase(req, res));
+app.get('/firestore', (req: Request, res: Response) => firebase(req, res));
 
 app.listen(port, () => {
   return console.log(`Express is listening at http://localhost:${port}`);
