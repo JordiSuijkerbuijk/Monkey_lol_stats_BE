fc976bb8f73bf72ca86feb1ad1a2288a654aa848
Roel <rvoordendag@gmail.com>
2022-03-03 19:21:43 +0100

    Cleaned up DB file

--- app/utils/db.ts
+++ app/utils/db.ts
@@ -2,7 +2,7 @@ const admin = require("firebase-admin");
 
 const serviceAccount = require("./../../firestoreKey.json");
 
-    admin.initializeApp({
+admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
 });
     
@@ -10,6 +10,7 @@ const DB = admin.firestore();
 
 module.exports = DB;
 
+// EXAMPLE GET ALL DATA;
 // db.collection('test').get().then((result:any) => {
 //     result.forEach((test:any) => {
 //         console.log(test.data());
