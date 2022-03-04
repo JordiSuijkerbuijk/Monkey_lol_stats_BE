8b749bbb995bd692cef7b5258e53e8b2fa89b9d4
Roel <rvoordendag@gmail.com>
2022-03-04 16:04:06 +0100

    created general riot util class

--- utils/riot.ts
+++ utils/riot.ts
@@ -1,15 +1,36 @@
-const fetch = require('node-fetch');
 
-module.exports = (apiKey:String) => {
-    getSummonerId: async (username:String) => {
-        const summonerData = await fetch(`https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/${username}`, {
+// module.exports = (apiKey:String) => {
+//     getSummonerId: async (username:String) => {
+//         const summonerData = await fetch(`https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/${username}`, {
+//             headers: {
+//             'X-Riot-Token': apiKey,
+//             },
+//         });
+          
+//         const summonerJson = await summonerData.json();
+          
+//         return summonerJson.puuid;          
+//     }
+// }
+
+export class RiotClass {
+    private apiKey:string;
+    private fetch:any;
+
+    constructor(riotApi:any){
+        this.apiKey = riotApi;
+        this.fetch = require('node-fetch');
+    }
+
+    public async getSummonerId (username:String){
+        const summonerData = await this.fetch(`https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/${username}`, {
             headers: {
-            'X-Riot-Token': apiKey,
+                'X-Riot-Token': this.apiKey,
             },
         });
-          
+                      
         const summonerJson = await summonerData.json();
-          
-        return summonerJson.puuid;          
+                      
+        return summonerJson.puuid;  
     }
 }
